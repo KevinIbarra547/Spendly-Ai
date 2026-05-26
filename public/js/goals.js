86b5566b8b511d3013f059cae01738710953d9a1
Kevin Ibarra <kevinibarrak02@gmail.com>
2026-05-26 11:18:56 -0700

    Remove CP04 test UI comments from goals.js
    Remove commented-out CP04 test UI code.

--- public/js/goals.js
+++ public/js/goals.js
@@ -1,4 +1,4 @@
-/* ===== CP04 TEST UI - REMOVE IN CP06 =====
+// ===== CP04 TEST UI START - REMOVE IN CP06 =====
 
 // Goals CRUD Testing UI
 const goalListDiv = document.getElementById('goal-list');
